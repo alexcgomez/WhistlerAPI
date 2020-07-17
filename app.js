af9637c08d8a60284f112a40bff48af2d35f70c7
Alex Caceres <alex.caceres@lambdaloopers.com>
2020-07-18 01:35:09 +0200

    API Running

---
+++ app.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const PORT = process.env.SERVER_PORT || 8080;
+const express = require('express');
+const app = express();
+
+dotenv.config();
+
+// Express middleware
+app.use(bodyParser.json());
+
+// DB Connection
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+// Test Connection
+connection.connect(error => {
+  if(error) throw error;
+  console.log('Database server running!');
+});
+
+// Route
+app.get('/', (req, res) => {
+  res.send('Welcome Whistle API');
+});
+
+// Serve
+app.listen(PORT, () => {
+  console.log('SERVER RUNNING ON PORT', PORT);
+});
