6ee84083e5abc176def287254c1468c674d7b22f
Alex Caceres <alex.caceres@lambdaloopers.com>
2020-08-01 01:34:15 +0200

    Server running on TypeScript

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
-const express = require('express');
+import { Request, Response } from 'express';
 require('dotenv').config({path: '../.env'});
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+// Express configuration
+app.set('port', PORT);
+
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello Server!');
+});
+
+app.listen(PORT, () => {
+  console.log('SERVER RUNNING ON PORT', PORT);
 });
