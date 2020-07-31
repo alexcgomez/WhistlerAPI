d5fda1a6f4cef58e3ad1d9f436d1c8d6466dc6ea
Alex Caceres <alex.caceres@lambdaloopers.com>
2020-08-01 01:17:47 +0200

    Installed linter

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,8 @@
-const dotenv = require('dotenv');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const PORT = process.env.SERVER_PORT || 8080;
 const express = require('express');
+require('dotenv').config({path: '../.env'});
+
 const app = express();
-const db = require('./db/models/index.js');
 
-dotenv.config();
-
-// Express middleware
-app.use(bodyParser.json());
-
-// DB Connection
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
-
-// Test Connection
-connection.connect(error => {
-  if(error) throw error;
-  console.log('Database server running!');
-});
-
-// Route
 app.get('/', (req, res) => {
-  res.send('Welcome Whistler API');
-});
-
-// Serve
-app.listen(PORT, () => {
-  console.log('SERVER RUNNING ON PORT', PORT);
+  res.send('Hello Server!');
 });
