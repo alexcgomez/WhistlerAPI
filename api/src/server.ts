70fcbf2369e349e7a53dec09c1992a5e0fc76ed3
Alex Caceres <alex.caceres@lambdaloopers.com>
2020-08-07 00:06:12 +0200

    Backend working dockerized

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,13 +6,13 @@ import * as morgan from 'morgan';
 
 dotenv.config({path: '../.env'});
 const {SERVER_PORT} = process.env;
-
 //DB
 createConnection().then(async () => {
 
 //EXPRESS
   const app = express();
-// Middlewares
+
+  // Middlewares
   app.use(cors());
   app.use(express.json());
   app.use(morgan('short'));
